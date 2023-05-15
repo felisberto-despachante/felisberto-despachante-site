2d7a80662aff7cf43742144cfc6b81b956c61d9b
Van-Romel A S Neto <vanr.neto@hotmail.com>
2023-05-15 17:48:14 -0300

    message

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
 import {RouterModule, Routes} from "@angular/router";
 import {NgModule} from "@angular/core";
-import {InicioComponent} from "./inicio/inicio.component";
-import {ServicosComponent} from "./servicos/servicos.component";
-import {ContatosComponent} from "./contatos/contatos.component";
-import {SobreNosComponent} from "./sobre-nos/sobre-nos.component";
-import {FormulariosComponent} from "./formularios/formularios.component";
-import {LinksComponent} from "./links/links.component";
+import {HomeComponent} from "./home/home.component";
+import {FuncionarioComponent} from "./funcionario/funcionario.component";
 
 const routes: Routes = [
-  {path: '', component: InicioComponent},
-  {path: 'servicos', component: ServicosComponent},
-  {path: 'contatos', component: ContatosComponent},
-  {path: 'sobre-nos', component: SobreNosComponent},
-  {path: 'formularios', component: FormulariosComponent},
-  {path: 'links', component: LinksComponent},
+  {path: '', component: HomeComponent},
+  {path: 'funcionario', component: FuncionarioComponent},
   {path: '**', redirectTo: ''}
 ]
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
+import {HomeComponent} from "./home/home.component";
+import {FuncionarioComponent} from "./funcionario/funcionario.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FuncionarioComponent
   ],
   imports: [
     BrowserModule,
