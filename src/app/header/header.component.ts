ff0c45197151cb3504f7573e7e8efe0d97b5b1ea
Van-Romel A S Neto <vanr.neto@hotmail.com>
2023-05-21 22:49:07 -0300

    Sou massa 😎

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,43 @@
+import {Component, OnInit, ChangeDetectorRef} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  currentPage!: string;
+  isDropdownOpen = false;
+  pages: string[] = ['home', 'arquivos'];
+
+  constructor(private router: Router, private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+    this.updateCurrentPage();
+
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateCurrentPage();
+      }
+    });
+  }
+
+  private updateCurrentPage() {
+    this.currentPage = this.router.url;
+    this.cdr.detectChanges();
+  }
+
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
+  }
+
+  selectOption(option: string) {
+    if (this.currentPage.toString() != '/' + option) {
+      this.currentPage = option;
+      this.isDropdownOpen = false;
+      this.router.navigate([option]);
+    }
+  }
+}
---
+++ src/app/sections/section-servicos/section-servicos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-section-servicos',
+  templateUrl: './section-servicos.component.html',
+  styleUrls: ['./section-servicos.component.scss']
+})
+export class SectionServicosComponent {
+
+}
