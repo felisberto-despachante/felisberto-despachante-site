ae64476ce7dc40fe1f895e4696e54d563ebe8a80
Van-Romel A S Neto <vanr.neto@hotmail.com>
2023-06-17 23:10:56 -0300

    Links de navegação funcionando diretamente para a section desejada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import {RouterModule, Routes} from "@angular/router";
+import {RouterModule, Routes, ExtraOptions} from "@angular/router";
 import {NgModule} from "@angular/core";
 import {HomeComponent} from "./home/home.component";
 import {ArquivosComponent} from "./arquivos/arquivos.component";
@@ -8,10 +8,14 @@ const routes: Routes = [
   {path: 'arquivos', component: ArquivosComponent},
   {path: '**', redirectTo: 'home'}
 ]
+const routerOptions: ExtraOptions = {
+  scrollPositionRestoration: "enabled",
+  anchorScrolling: "enabled"
+}
 
 @NgModule({
   declarations: [],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, routerOptions)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
